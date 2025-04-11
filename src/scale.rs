6d8b7a75ed8d4b6f1733716584efcbdd08b866c8
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-04-10 22:27:50 -0400

    not a single scale yet lol

--- src/scale.rs
+++ src/scale.rs
@@ -1,3 +1,7 @@
+pub const CHROMATIC_SCALE: [&str; 12] = [
+    "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
+];
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum ScaleType {
     Major,
@@ -8,10 +12,23 @@ pub enum ScaleType {
     MinorPentatonic,
 }
 
+fn get_intervals(scale_type: &ScaleType) -> Vec<i32> {
+    match scale_type {
+        ScaleType::Major => vec![0, 2, 4, 5, 7, 9, 11],
+        ScaleType::Minor => vec![0, 2, 3, 5, 7, 8, 10],
+        ScaleType::MinorMelodic => vec![0, 2, 3, 5, 7, 9, 11],
+        ScaleType::MinorHarmonic => vec![0, 2, 3, 5, 7, 8, 11],
+        ScaleType::MajorPentatonic => vec![0, 2, 4, 7, 9],
+        ScaleType::MinorPentatonic => vec![0, 3, 5, 7, 10],
+    }
+}
 
-pub struct Scale {
-    // root: input,
-    // scale_type: ScaleType,
-    // intervals: intervals
+fn get_scale_notes() {
+
+    let tonic = CHROMATIC_SCALE;
 
 }
+
+pub struct Scale {}
+
+// (T-T-ST-T-T-T-ST)
